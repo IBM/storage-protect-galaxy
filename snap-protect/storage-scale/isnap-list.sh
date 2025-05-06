02f53c7dd94b1a609e0cebefd37f402e7fa17daa
Nils Haustein <nils_haustein@de.ibm.com>
2025-05-06 14:47:20 +0200

    parse config parameter sudoCommand from config file - version 1.2.1

--- snap-protect/storage-scale/isnap-list.sh
+++ snap-protect/storage-scale/isnap-list.sh
@@ -26,7 +26,9 @@
 #   -h | --help:            Show this help message (optional).
 # 
 #********************************************************************************
-
+#
+# History
+# 04/30/25 added sudoCmd to snapconfig.json - version 1.2.1
 
 #---------------------------------------
 # global parameters
@@ -41,8 +43,8 @@ gpfsPath="/usr/lpp/mmfs/bin"
 # determine the name of the instance user for reference
 instUser=$(id -un)
 
-# sudo command to be used
-sudoCmd=/usr/bin/sudo
+# version of the program
+ver="1.2.1"
 
 
 #------------------------------------------------------------------
@@ -112,13 +114,16 @@ function parse_config()
             if [[ "$name" = "dirsToSnap" ]]; then
               dirsToSnap=$val
             fi
-			if [[ "$name" = "apiServerIP" ]]; then
+            if [[ "$name" = "sudoCommand" ]]; then
+              sudoCmd=$val
+            fi
+			      if [[ "$name" = "apiServerIP" ]]; then
               apiServer=$val
             fi
-			if [[ "$name" = "apiServerPort" ]]; then
+			      if [[ "$name" = "apiServerPort" ]]; then
               apiPort=$val
             fi
-			if [[ "$name" = "apiCredentials" ]]; then
+			      if [[ "$name" = "apiCredentials" ]]; then
               apiAuth=$val
             fi
           fi
@@ -178,6 +183,10 @@ function list_apisnapshot()
 # Main
 #---------------------------------------
 
+# present banner
+echo -e "\n============================================================================================="
+echo -e "INFO: $(date) program $0 version $ver started by $instUser\n"
+
 # parse arguments from the command line
 verbose=0
 snapName=""
@@ -216,10 +225,13 @@ if [[ ! -a $configFile ]]; then
 fi
 dirsToSnap=""
 snapPrefix=""
+sudoCmd="/usr/bin/sudo"
 apiServer=""
 apiPort=""
 apiAuth=""
 parse_config
+#echo -e "DEBUG: Snapshot configuration from $configFile:\n  dbName=$dbName\n  dirsToSnap=$dirsToSnap\n  snapPrefix=$snapPrefix\n  snapRet=$snapRet\n  serverInstDir=$serverInstDir\n  sudoCommand=$sudoCmd\n  apiServer=$apiServer\n  apiPort=$apiPort\n  apiAuth=$apiAuth\n"
+
 
 
 ### Check required parameters
@@ -311,7 +323,5 @@ do
    fi
 done
 
-echo
-echo "============================================================================================"
 echo
 exit 0
